d04be19f7245d7384a196d9758205f83c3377810
Volodya <VladimirMozhenkov@yahoo.com>
2025-06-24 09:54:17 +0300

    [minor] Spaces to a tab

--- classes/HtmlSnippets.php
+++ classes/HtmlSnippets.php
@@ -3,7 +3,7 @@
 class HtmlSnippets
 {
 	public static function __callStatic(string $method, array $parameters)
-    {
+	{
 		if(str_starts_with($method, 'print'))
 		{
 			call_user_func_array("self::{$method}", $parameters);
