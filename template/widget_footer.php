796ffd26abb5caccb27470d04808f12e357942ac
Volodya <VladimirMozhenkov@yahoo.com>
2025-06-27 11:38:30 +0300

    footer

--- template/widget_footer.php
+++ template/widget_footer.php
@@ -4,6 +4,7 @@
 		Code is available under <a href='https://www.gnu.org/licenses/gpl-3.0.en.html'>GNU GPL 3.0 (or later)</a>.
 		Scans of Postcards are assumed to be Fair Use to demonstrate the cards.
 		User profiles are copyrighted by the users.
+		Current telegraph timestamp is <?php HtmlSnippets::printTelegraphTimestamp(date('Y-m-d H:i:s')) ?>.
 	</div>
 	<div class='cookies'>This site uses cookies to enable login, dynamic content, and other functionality.</div>
 </footer>
