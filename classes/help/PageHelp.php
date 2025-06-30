ef41a14cc347828ec1ed49676e190e58239b48a1
Volodya <VladimirMozhenkov@yahoo.com>
2025-06-30 15:33:23 +0300

    Note about TG group on the help page

--- classes/help/PageHelp.php
+++ classes/help/PageHelp.php
@@ -20,6 +20,12 @@ class PageHelp extends Page_Abstract
 					[
 						['gethelp'],
 						['help_info', 'make_section' => true],
+						[
+							'text',
+							'make_section' => true,
+							'section_header' => 'Community help',
+							'parameter' => 'You talk to other users of this site via a <a href="https://t.me/HappyPostcard">Telegram group</a>.'
+						],
 					]
 				)
 				->withBottom(
