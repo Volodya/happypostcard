7c7dc05d5d8b7b4f6bbfcd27e5e41117ad7d72cb
Volodya <VladimirMozhenkov@yahoo.com>
2025-07-11 08:58:06 +0300

    Site description as a new style widget

---
+++ classes/ComWid_sitedescription.php
@@ -0,0 +1,31 @@
+<?php
+
+class ComWid_sitedescription extends SimpWid
+{
+	public function invoke() : void
+	{
+		?><section><?php
+			?><h1>Postcard exchange</h1><?php
+				?><p>
+					Postcard exchange (sometimes called &ldquo;Postcrossing&rdquo; after the first website that was created to fascilitate
+					this hobby) allows people to send and receive real postcards. The joy of this card is the ability to connect with a
+					random person somewhere around the world, which is very important in today&apos;s world, where an e-mail is considered
+					the least technical means of communicating, and many of us feel very isolated from one another.
+				</p><?php
+		?></section><?php
+		?><section><?php
+			?><h1>Happy Postcard</h1><?php
+			?><p>
+				This project does not seek to compete with other similar websites. We try to enhanse the ability of everybody to
+				exchange postcards. Some of the unique features of this site include:
+			</p><?php
+			?><ul><?php
+				?><li>send the card representing not only your country, but your particular region or the whole world,</li><?php
+				?><li>send a &laquo;Happy Birthday&raquo; card to a person without worrying about the limit,</li><?php
+				?><li>upload the image of the card as the sender and as a receiver,</li><?php
+				?><li>send the card to the person, you have received a card from,</li><?php
+				?><li>send a postcard to a person, who is in a difficult situation, and will probably not send anything back (coming soon).</li><?php
+			?></ul><?php
+		?></section><?php
+	}
+}
